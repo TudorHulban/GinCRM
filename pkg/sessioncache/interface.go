0841e12ef6e047146fd59bd19b50cf086b8b3266
Tudor Hulban <tudi@J3710>
2020-09-01 18:39:51 +0300

    wrap

--- pkg/sessioncache/interface.go
+++ pkg/sessioncache/interface.go
@@ -3,7 +3,7 @@ package sessioncache
 // ISessionCache is the interface proposed for interacting with a session ID cache.
 type ISessionCache interface {
 	InsertSession(sessionID int64)                       // InsertSession inserts session ID
-	InsertSessionWithTTL(sessionID int64, ttlSecs int64) // InsertSession inserts session ID with time to live in seconds
+	InsertSessionWithTTL(sessionID int64, ttlSecs int64) // InsertSessionWithTTL inserts session ID with time to live in seconds
 	FoundSessionID(sessionID int64) bool                 // FoundSessionID returns true if passed session ID is found
 	DeleteSession(sessionID int64)                       // DeleteSession deletes passed session ID
 }
