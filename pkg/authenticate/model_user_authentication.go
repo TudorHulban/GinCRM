ac4bef2d2001c517e06658e9c109420db60eeeff
Tudor Hulban <tudi@J3710>
2020-09-03 16:57:02 +0300

    added atuh

---
+++ pkg/authenticate/model_user_authentication.go
@@ -0,0 +1,37 @@
+package autentication
+
+import (
+	"github.com/TudorHulban/GinCRM/pkg/cache/cachelogin"
+	"github.com/TudorHulban/badgerwrap"
+	"github.com/pkg/errors"
+)
+
+// UserAuth Concentrates user authentication based on password.
+type UserAuth struct {
+	Code     string
+	Password string
+}
+
+// SaveToCache Method saves user to login cache.
+func (u UserAuth) SaveToCache() error {
+	return cachelogin.GetCache().Set(badgerwrap.KV{
+		Key:   []byte(u.Code),
+		Value: []byte(u.Password),
+	})
+}
+
+// IsAuthenticated Method checks if credentials match the cache.
+func (u UserAuth) IsAuthenticated(usercode, password string) error {
+	pass, errGet := cachelogin.GetCache().GetVByK([]byte(usercode))
+	if errGet != nil {
+		return errors.WithMessage(errGet, "error when checking login cache")
+	}
+
+	if pass != []byte(password) {
+		return errors.New("password does not match login cache one")
+	}
+}
+
+func (u UserAuth) DeleteFromCache(usercode string) error {
+	return cachelogin.GetCache().DeleteKVByK([]byte(usercode))
+}
